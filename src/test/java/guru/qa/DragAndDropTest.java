4eb9615a4ec2d45c43c6490d8b97bed4bd7a8488
igorsid <sidgarri@gmail.com>
2022-05-07 12:17:37 +0800

    Add SelenideTest and DragAndDropTest

---
+++ src/test/java/guru/qa/DragAndDropTest.java
@@ -0,0 +1,20 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+
+public class DragAndDropTest {
+    @Test
+            void changeElement() {
+        //Открыть сайт
+        Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
+        // Перенесим прямоугольник А на место В
+        $("#column-a").dragAndDropTo("#column-b");
+        // Проверяем как прямоугольники поменялись местами
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
